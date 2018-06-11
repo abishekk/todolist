c6f42c72c79e6a18363616bdddbd611a4c7340d4
Abi <1175247+abishekk@users.noreply.github.com>
2018-06-11 01:27:46 -0400

    Add a simple button from blueprintjs. Considering dropping the whole blueprint idea for the todolist. Instead prefer atlas kit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button, Intent, Spinner, Dialog } from "@blueprintjs/core";
 import './App.css';
 
 class App extends Component {
+  state = {
+    dialog: {
+      autoFocus: true,
+      canEscapeKeyClose: true,
+      canOutsideClickClose: true,
+      enforceFocus: true,
+      isOpen: false,
+      usePortal: true
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <Spinner intent={Intent.PRIMARY} />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <Button intent={Intent.SUCCESS} onClick={this.handleOnButtonClick}>Hello World!</Button>
+          <Dialog icon="info-sign" onClose={this.handleOnClose} {...this.state.dialog} >Hello </Dialog>
         </p>
       </div>
     );
   }
+  handleOnButtonClick = () => {this.setState({dialog: {...this.state.dialog, isOpen: true}})}
+  handleOnClose = () => {this.setState({dialog: {...this.state.dialog, isOpen: false}})}
 }
 
 export default App;
